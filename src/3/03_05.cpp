0f977c188427c0881fafd84546228cfb88b09f17
asharykin <aleksandr-sharykin@mail.ru>
2025-11-20 00:22:50 +0300

    03_05

--- src/3/03_05.cpp
+++ src/3/03_05.cpp
@@ -1,61 +1,74 @@
 #include <assert.h>
 
-class Entity_v1 {
+class Entity_v1
+{
 public:
     virtual ~Entity_v1() = default;
 
-    virtual int test() {
+    virtual int test()
+    {
         return 1;
     }
 };
 
-class Entity_v2 {
+class Entity_v2
+{
 public:
     virtual ~Entity_v2() = default;
 
-    virtual int test() {
+    virtual int test()
+    {
         return 2;
     }
 };
 
-class Adapter_v1 : public Entity_v1 {
+class Adapter_v1 : public Entity_v1
+{
 protected:
-    virtual int test_v1() {
+    virtual int test_v1()
+    {
         return 11;
     }
 
 public:
     ~Adapter_v1() = default;
 
-    int test() override {
+    int test() override
+    {
         return test_v1();
     }
 };
 
-class Adapter_v2 : public Entity_v2 {
+class Adapter_v2 : public Entity_v2
+{
 protected:
-    virtual int test_v2() {
+    virtual int test_v2()
+    {
         return 22;
     }
 
 public:
     ~Adapter_v2() = default;
 
-    int test() override {
+    int test() override
+    {
         return test_v2();
     }
 };
 
 
-class Client : public Adapter_v1, public Adapter_v2 {
+class Client : public Adapter_v1, public Adapter_v2
+{
 public:
     ~Client() = default;
 
-    int test_v1() override {
+    int test_v1() override
+    {
         return Adapter_v1::test_v1() + 1;
     }
 
-    int test_v2() override {
+    int test_v2() override
+    {
         return Adapter_v2::test_v2() + 2;
     }
 };
