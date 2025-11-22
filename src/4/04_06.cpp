e857d85e09086139429c37f16498bfa330999373
asharykin <aleksandr-sharykin@mail.ru>
2025-11-22 16:21:12 +0300

    04_06

--- src/4/04_06.cpp
+++ src/4/04_06.cpp
@@ -1,26 +1,42 @@
 #include <array>
+#include <cmath>
 
-consteval double compute_e(double eps)
-{
+consteval double compute_e(double eps) {
     double m = 1.0;
     double e = 0.0;
-    for (int i = 1; m >= eps; ++i)
-    {
+    for (int i = 1; m >= eps; ++i) {
         e += m;
         m /= i;
     }
     return e;
 }
 
-int main()
-{
-    constexpr std::array epsilons = {0.01, 0.001, 0.0001};
+consteval double compute_pi(double eps) {
+    double m = 1.0;
+    double pi = 0.0;
+    for (int i = 1; std::fabs(m) >= eps; ++i) {
+        pi += 4 * m;
+        m = (i % 2 == 0 ? 1.0 : -1.0) / (2 * i + 1);
+    }
+    return pi;
+}
+
+int main() {
+    constexpr std::array epsilons = {0.01, 0.0001, 0.00001};
 
     constexpr double e1 = compute_e(epsilons[0]);
     constexpr double e2 = compute_e(epsilons[1]);
     constexpr double e3 = compute_e(epsilons[2]);
 
     static_assert(e1 > 2.7 && e1 < 2.8);
-    static_assert(e2 > 2.71 && e2 < 2.72);
-    static_assert(e3 > 2.718 && e3 < 2.719);
+    static_assert(e2 > 2.718 && e2 < 2.719);
+    static_assert(e3 > 2.7182 && e3 < 2.7183);
+
+    constexpr double pi1 = compute_pi(epsilons[0]);
+    constexpr double pi2 = compute_pi(epsilons[1]);
+    constexpr double pi3 = compute_pi(epsilons[2]);
+
+    static_assert(pi1 > 3.1 && pi1 < 3.2);
+    static_assert(pi2 > 3.141 && pi2 < 3.142);
+    static_assert(pi3 > 3.1415 && pi3 < 3.1416);
 }
