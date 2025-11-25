6266a974a0f17d61f29b2b09433fc5831f56d22c
asharykin <aleksandr-sharykin@mail.ru>
2025-11-26 00:09:41 +0300

    02_08

--- src/2/02_08.cpp
+++ src/2/02_08.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
 
-std::size_t collatz_length(unsigned long long int i, std::vector<std::size_t>& cache)
+std::size_t compute_collatz_length_for_i(unsigned long long int i, std::vector<std::size_t>& cache)
 {
-    unsigned long long n = i;
     std::size_t length = 0;
+    unsigned long long n = i;
+
     while (n != 1 && n >= i)
     {
         if (n % 2 == 0)
@@ -17,22 +18,23 @@ std::size_t collatz_length(unsigned long long int i, std::vector<std::size_t>& c
         }
         length++;
     }
-    length += cache[n];
-    cache[i] = length;
+
+    length += cache[n - 1];
+    cache[i - 1] = length;
     return length;
 }
 
 int main() {
-    unsigned long long limit = 100;
     std::size_t max_length = 0;
     unsigned long long max_length_n = 0;
-    std::vector<std::size_t> cache(limit + 1, 0);
-    for (unsigned long long i = 2; i <= limit; i++)
+
+    std::vector<std::size_t> cache(100, 1);
+    for (unsigned long long i = 1; i <= 100; i++)
     {
-        collatz_length(i, cache);
-        if (cache[i] > max_length)
+        compute_collatz_length_for_i(i, cache);
+        if (cache[i - 1] > max_length)
         {
-            max_length = cache[i];
+            max_length = cache[i - 1];
             max_length_n = i;
         }
     }
